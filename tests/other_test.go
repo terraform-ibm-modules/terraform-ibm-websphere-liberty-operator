a9ba4f283df937f14407a2fab4c4dd1d7353f2d5
Rajat Agrawal <rajatatibm@gmail.com>
2024-02-08 17:45:28 +0000

    feat: catalog onboarding

--- tests/other_test.go
+++ tests/other_test.go
@@ -1,7 +1,12 @@
 // Tests in this file are NOT run in the PR pipeline. They are run in the continuous testing pipeline along with the ones in pr_test.go
 package test
 
-import "github.com/stretchr/testify/assert"
+import (
+	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestRunCompleteExample(t *testing.T) {
 	t.Parallel()
