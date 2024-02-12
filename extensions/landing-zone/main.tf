ddb3010ddfd3fba2e84ac0e4d85584df529a7beb
valerio-bontempi <valerio.bontempi@it.ibm.com>
2024-02-12 16:14:50 +0100

    fix: testing kubeconfig in extension

--- extensions/landing-zone/main.tf
+++ extensions/landing-zone/main.tf
@@ -5,7 +5,7 @@
 data "ibm_container_cluster_config" "cluster_config" {
   cluster_name_id = var.cluster_id
   config_dir      = "${path.module}/kubeconfig"
-  # endpoint_type   = var.cluster_config_endpoint_type != "default" ? var.cluster_config_endpoint_type : null # null represents default
+  endpoint_type   = var.cluster_config_endpoint_type != "default" ? var.cluster_config_endpoint_type : null # null represents default
 }
 
 module "websphere_liberty_operator" {
