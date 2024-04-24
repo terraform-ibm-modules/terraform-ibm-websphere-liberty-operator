9de7aa2cba119d95a4a8c6cddc1195767c4d6d73
valerio-bontempi <valerio.bontempi@it.ibm.com>
2024-04-24 18:40:38 +0200

    fix: fixed tags parameter

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -3,8 +3,8 @@
 ##############################################################################
 
 module "landing_zone" {
-  source = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks-quickstart?ref=v5.21.0"
-  region = var.region
-  prefix = var.prefix
-  tags   = var.resource_tags
+  source        = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks-quickstart?ref=v5.21.0"
+  region        = var.region
+  prefix        = var.prefix
+  resource_tags = var.resource_tags
 }
