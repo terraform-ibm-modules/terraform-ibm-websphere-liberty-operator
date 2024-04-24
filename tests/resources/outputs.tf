bb37e5859f8a8b3c7b8fa0b9c68b9373173db759
valerio-bontempi <valerio.bontempi@it.ibm.com>
2024-04-24 16:47:41 +0200

    feat: moved to ROKS quickstart flavor the SLZ test

--- tests/resources/outputs.tf
+++ tests/resources/outputs.tf
@@ -13,15 +13,11 @@ output "region" {
 }
 
 output "management_cluster_id" {
-  value = lookup(
-    [for cluster in module.landing_zone.cluster_data : cluster if strcontains(cluster.resource_group_name, "management")][0], "id", ""
-  )
+  value       = module.landing_zone.management_cluster_id
   description = "management cluster ID."
 }
 
 output "workload_cluster_id" {
-  value = lookup(
-    [for cluster in module.landing_zone.cluster_data : cluster if strcontains(cluster.resource_group_name, "workload")][0], "id", ""
-  )
+  value       = module.landing_zone.workload_cluster_id
   description = "workload cluster ID."
 }
