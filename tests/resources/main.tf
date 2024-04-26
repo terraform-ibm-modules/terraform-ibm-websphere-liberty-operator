512689886cf26793d56d983216bb753fc5f29a6f
valerio-bontempi <valerio.bontempi@it.ibm.com>
2024-04-26 15:46:05 +0200

    fix: added apikey input

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -3,8 +3,9 @@
 ##############################################################################
 
 module "landing_zone" {
-  source        = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks-quickstart?ref=v5.21.0"
-  region        = var.region
-  prefix        = var.prefix
-  resource_tags = var.resource_tags
+  source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks-quickstart?ref=v5.21.0"
+  ibmcloud_api_key = var.ibmcloud_api_key
+  region           = var.region
+  prefix           = var.prefix
+  resource_tags    = var.resource_tags
 }
