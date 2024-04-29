9cc751375e66d9fba54ae5b81a92bb93a2bfdd10
valerio-bontempi <valerio.bontempi@it.ibm.com>
2024-04-29 17:02:38 +0200

    fix: added ignore updates for catalog image upd

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -33,9 +33,8 @@ func setupOptions(t *testing.T, prefix string, exampleDir string) *testhelper.Te
 			List: []string{
 				// to skip update error due to operator sample app updates
 				"module.websphere_liberty_operator.helm_release.websphere_liberty_operator_sampleapp[0]",
-				// to avoid upgrade to fail when sleep time is reduced
-				// remove this ignore once https://github.com/terraform-ibm-modules/terraform-ibm-websphere-liberty-operator/pull/31 is merged
-				"module.websphere_liberty_operator.time_sleep.wait_sampleapp[0]",
+				// to skip update error due to operator catalog image version updates
+				"module.websphere_liberty_operator.helm_release.ibm_operator_catalog[0]",
 			},
 		},
 	})
