e62b9f8404e8eeab8fc6012647358611bf17de00
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-08-31 05:54:15 +0000

    fix(deps): update icr.io/cpopen/ibm-operator-catalog:v4.16 docker digest to d0c83a5

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "kubernetes_namespace" "helm_release_operator_namespace" {
 }
 
 locals {
-  ibm_operator_catalog_image_tag_digest = "v4.16@sha256:6faeb20ed88700f79c90803eddb6f11bfa16d3513dadfbd85ff456ed1e7aa68d" # datasource: icr.io/cpopen/ibm-operator-catalog
+  ibm_operator_catalog_image_tag_digest = "v4.16@sha256:d0c83a50a36fc8a3ab34fc0c5493c9d7d5b0c8616a3dabde381627a37151061d" # datasource: icr.io/cpopen/ibm-operator-catalog
   ibm_operator_catalog_path             = "icr.io/cpopen/ibm-operator-catalog"
 }
 
