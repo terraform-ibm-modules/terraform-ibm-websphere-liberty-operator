1643383898a34bd9745e1fb97e9de562a99eaf60
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-09-07 20:06:32 +0100

    chore(deps): update terraform-module (#123)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -75,7 +75,7 @@ locals {
 module "ocp_base" {
   depends_on           = [ibm_is_vpc.vpc, ibm_is_subnet.cluster_subnets, ibm_is_public_gateway.gateway]
   source               = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version              = "3.29.3"
+  version              = "3.30.0"
   cluster_name         = "${var.prefix}-cluster"
   cos_name             = "${var.prefix}-cos"
   resource_group_id    = module.resource_group.resource_group_id
