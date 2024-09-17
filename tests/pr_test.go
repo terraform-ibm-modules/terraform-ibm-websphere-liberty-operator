2ef1b3f6c6fdc52ce438e6fb8aaa2bca705ee247
Valerio Bontempi <valerio.bontempi@it.ibm.com>
2024-09-17 16:02:07 +0200

    test: Fixed workaround for default pool management in tests (#132)

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -35,11 +35,13 @@ func setupOptions(t *testing.T, prefix string, exampleDir string) *testhelper.Te
 				"module.websphere_liberty_operator.helm_release.websphere_liberty_operator_sampleapp[0]",
 				// to skip update error due to operator catalog image version updates
 				"module.websphere_liberty_operator.helm_release.ibm_operator_catalog[0]",
-				// workaround for the issue https://github.ibm.com/GoldenEye/issues/issues/10743
-				// when the issue is fixed on IKS, so the destruction of default workers pool is correctly managed on provider/clusters service the next two entries should be removed
-				"module.ocp_base.ibm_container_vpc_worker_pool.autoscaling_pool[\"default\"]",
-				"module.ocp_base.ibm_container_vpc_worker_pool.pool[\"default\"]",
 			},
+		},
+		ImplicitDestroy: []string{
+			// workaround for the issue https://github.ibm.com/GoldenEye/issues/issues/10743
+			// when the issue is fixed on IKS, so the destruction of default workers pool is correctly managed on provider/clusters service the next two entries should be removed
+			"module.ocp_base.ibm_container_vpc_worker_pool.autoscaling_pool[\"default\"]",
+			"module.ocp_base.ibm_container_vpc_worker_pool.pool[\"default\"]",
 		},
 	})
 	return options
