a8d6d5b6fa565157a689b04db96ce06fe6f3c7cc
ocofaigh <ocofaigh@ie.ibm.com>
2024-09-26 09:53:26 +0100

    fix for tests

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -3,7 +3,7 @@
 ##############################################################################
 
 module "landing_zone" {
-  source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks-quickstart?ref=v6.0.0"
+  source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks-quickstart?ref=v6.0.1"
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.region
   prefix           = var.prefix
@@ -11,4 +11,6 @@ module "landing_zone" {
   # GHA runtime has no access to private endpoints, so set these to false
   verify_cluster_network_readiness    = false
   use_ibm_cloud_private_api_endpoints = false
+  # allow outbound traffic so images can be pulled from public
+  disable_outbound_traffic_protection = false
 }
