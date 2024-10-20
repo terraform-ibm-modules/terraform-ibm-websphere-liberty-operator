ff921ebce31be4882f14bdd2b40021404190efff
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-10-20 17:44:22 +0000

    chore(deps): update terraform github.com/terraform-ibm-modules/terraform-ibm-landing-zone to v6.1.1

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -3,7 +3,7 @@
 ##############################################################################
 
 module "landing_zone" {
-  source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks-quickstart?ref=v6.1.0"
+  source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks-quickstart?ref=v6.1.1"
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.region
   prefix           = var.prefix
