1de8755a932aa41489d7f9026ee0ce41694c07ee
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-02-27 10:10:52 +0530

    fix: remove workaround for default pool management

--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -3,6 +3,7 @@
 ##############################################################################
 
 module "landing_zone" {
+  # TODO: Update the slz version once https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone/pull/960 is merged.
   source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks-quickstart?ref=v7.2.2"
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.region
