7c210d5a1c23dd4f9ddaf838f9e4ff32668a1050
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-08-03 00:19:41 +0100

    chore(deps): update terraform dependencies (#248)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -4,7 +4,7 @@
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.2.1"
+  version = "1.3.0"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -76,7 +76,7 @@ locals {
 module "ocp_base" {
   depends_on           = [ibm_is_vpc.vpc, ibm_is_subnet.cluster_subnets, ibm_is_public_gateway.gateway]
   source               = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version              = "3.53.3"
+  version              = "3.54.2"
   cluster_name         = "${var.prefix}-cluster"
   cos_name             = "${var.prefix}-cos"
   resource_group_id    = module.resource_group.resource_group_id
--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -3,7 +3,7 @@
 ##############################################################################
 
 module "landing_zone" {
-  source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks-quickstart?ref=v8.4.0"
+  source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks-quickstart?ref=v8.4.3"
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.region
   prefix           = var.prefix
