9a8ed6cfe6ae5173e3d277221b43c5d04d6d6399
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-08-24 23:10:14 +0000

    fix(deps): update dependencies

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -76,7 +76,7 @@ locals {
 module "ocp_base" {
   depends_on           = [ibm_is_vpc.vpc, ibm_is_subnet.cluster_subnets, ibm_is_public_gateway.gateway]
   source               = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version              = "3.54.2"
+  version              = "3.55.5"
   cluster_name         = "${var.prefix}-cluster"
   cos_name             = "${var.prefix}-cos"
   resource_group_id    = module.resource_group.resource_group_id
--- tests/resources/main.tf
+++ tests/resources/main.tf
@@ -3,7 +3,7 @@
 ##############################################################################
 
 module "landing_zone" {
-  source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks-quickstart?ref=v8.4.3"
+  source           = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone//patterns//roks-quickstart?ref=v8.5.5"
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.region
   prefix           = var.prefix
